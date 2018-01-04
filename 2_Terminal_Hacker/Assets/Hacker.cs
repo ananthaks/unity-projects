1c090710834e2b2b8aeb1097e7b0d9ea9ac557da
ananthaks <anantha.aks123@gmail.com>
2018-01-04 18:52:55 -0500

    Adding User input to terminal hacker game

--- 2_Terminal_Hacker/Assets/Hacker.cs
+++ 2_Terminal_Hacker/Assets/Hacker.cs
@@ -6,14 +6,29 @@ public class Hacker : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+        var greetings = "Anantha";
+        ShowMainMenu(greetings);
+	}
+
+    void ShowMainMenu(string msg) {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("> Hello "+msg);
         Terminal.WriteLine("> What would you like to hack into?");
         Terminal.WriteLine(">   Press 1 for EASY level");
         Terminal.WriteLine(">   Press 2 for MEDIUM level");
         Terminal.WriteLine(">   Press 3 for HARD level");
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    void OnUserInput(string message) {
+        Terminal.WriteLine("You wrote ");
+        Terminal.WriteLine(message);
+        //print(message);
+        print(message == "1");
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
