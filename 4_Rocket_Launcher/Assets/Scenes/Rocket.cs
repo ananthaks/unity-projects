357ccb34176ec40f275703c039467e9f81a2d4ef
ananthaks <anantha.aks123@gmail.com>
2018-09-02 13:49:24 -0400

    Added Basic control, sound to rocket

---
+++ 4_Rocket_Launcher/Assets/Scenes/Rocket.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody m_rigidBody;
+    private AudioSource m_audioSource;
+
+    //--------------------------------------
+    // Start
+    //
+    // Initialization
+    //--------------------------------------
+    private void Start ()
+    {
+        m_rigidBody = GetComponent<Rigidbody>();
+        m_audioSource = GetComponent<AudioSource>();
+    }
+
+    //--------------------------------------
+    // Update
+    //
+    // Called once per frame
+    //--------------------------------------
+    private void Update ()
+    {
+        HandleEvent();
+	}
+
+    //--------------------------------------
+    // HandleEvent
+    //
+    // Handle external interactions on rocket
+    //--------------------------------------
+    private void HandleEvent()
+    {
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) // Thrust
+        {
+            m_rigidBody.AddRelativeForce(Vector3.up);
+            if(!m_audioSource.isPlaying)
+            {
+                m_audioSource.Play();
+            }
+        }
+        else
+        {
+            m_audioSource.Stop();
+        }
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) // Rotate Left
+        {
+            m_rigidBody.AddRelativeTorque(Vector3.forward);
+        }
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) // Rotate Right
+        {
+            m_rigidBody.AddRelativeTorque(Vector3.back);
+        }
+    }
+}
